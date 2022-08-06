decc69648aa0da81c1b362ded98201d9eb92f014
James <jamesfang888@gmail.com>
2022-08-06 22:42:46 +1000

    Break out the cloud API functionalaties from the cloud data source in preparation for v2 API client

---
+++ RemarkableSync/Interfaces/ICloudApiClient.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace RemarkableSync
+{
+    interface ICloudApiClient : IDisposable
+    {
+        Task<List<RmItem>> GetAllItems();
+
+        Task<RmDownloadedDoc> DownloadDocument(RmItem item);
+
+    }
+}
